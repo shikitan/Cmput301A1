bb5c7c107465a1fcbf6f8daf7b5c1c0a61a8ac2d
shikitan <dshin@ualberta.ca>
2015-10-02 02:03:24 -0600

    Played around with the UI. just getting started

--- DanielBuzzer/app/src/main/java/com/example/user/danielbuzzer/MainActivity.java
+++ DanielBuzzer/app/src/main/java/com/example/user/danielbuzzer/MainActivity.java
@@ -1,9 +1,28 @@
+/*
+    DanielBuzzer: SUMMARY
+    Copyright 2015 Daniel Shin dshin@ualberta.ca
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
 package com.example.user.danielbuzzer;
 
+import android.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,11 +32,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+
     }
 
     @Override
