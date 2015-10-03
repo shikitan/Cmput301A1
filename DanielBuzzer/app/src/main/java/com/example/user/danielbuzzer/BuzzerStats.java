e42043deee0cc7e3f813a59b8d6ec9d2425d7df1
shikitan <dshin@ualberta.ca>
2015-10-03 01:16:30 -0600

    Saving before changing name

--- DanielBuzzer/app/src/main/java/com/example/user/danielbuzzer/BuzzerStats.java
+++ DanielBuzzer/app/src/main/java/com/example/user/danielbuzzer/BuzzerStats.java
@@ -1,41 +1,22 @@
 package com.example.user.danielbuzzer;
 
-import android.app.TabActivity;
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TabHost;
 
-public class Stats extends TabActivity {
-
-
+public class BuzzerStats extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_stats);
-
-        TabHost th = (TabHost)findViewById(android.R.id.tabhost);
-        th.setup();
-
-        TabHost.TabSpec spec = th.newTabSpec("tab1");
-        spec.setContent(R.id.ReactionStats);
-        spec.setIndicator("Reaction Stats");
-        th.addTab(spec);
-
-        spec = th.newTabSpec("tab2");
-        spec.setContent(R.id.BuzzerStats);
-        spec.setIndicator("Buzzer Stats");
-        th.addTab(spec);
-
+        setContentView(R.layout.activity_buzzer_stats);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_stats, menu);
+        getMenuInflater().inflate(R.menu.menu_buzzer_stats, menu);
         return true;
     }
 
