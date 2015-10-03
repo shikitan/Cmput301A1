693f5a44fc0f0398e6c0198c242c4bcac6497563
shikitan <dshin@ualberta.ca>
2015-10-02 22:14:12 -0600

    now creating a tab. finished the button links in gameshow

--- DanielBuzzer/app/src/main/java/com/example/user/danielbuzzer/MultiActivity.java
+++ DanielBuzzer/app/src/main/java/com/example/user/danielbuzzer/MultiActivity.java
@@ -1,16 +1,29 @@
 package com.example.user.danielbuzzer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
-public class MultiActivity extends AppCompatActivity {
+public class MultiActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Button button2p, button3p, button4p;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_multi);
+        button2p = (Button)findViewById(R.id.button2p);
+        button2p.setOnClickListener(this);
+
+        button3p = (Button)findViewById(R.id.button3p);
+        button3p.setOnClickListener(this);
+
+        button4p = (Button)findViewById(R.id.button4p);
+        button4p.setOnClickListener(this);
     }
 
     @Override
@@ -33,5 +46,30 @@ public class MultiActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    private void twopClick() {
+        startActivity(new Intent("danielbuzzer.Multi2"));
+    }
+    private void threepClick() {
+        startActivity(new Intent("danielbuzzer.Multi3"));
+    }
+    private void fourpClick() {
+        startActivity(new Intent("danielbuzzer.Multi4"));
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.button2p:
+                twopClick();
+                break;
+            case R.id.button3p:
+                threepClick();
+                break;
+            case R.id.button4p:
+                fourpClick();
+                break;
+        }
+
     }
 }
