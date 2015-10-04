e051d4b1434800510ec79ea8d975fae48d3bcc0e
shikitan <dshin@ualberta.ca>
2015-10-04 11:07:12 -0600

    going to extend activities

--- app/src/main/java/com/example/user/danielbuzzer/ReactionTime.java
+++ app/src/main/java/com/example/user/danielbuzzer/ReactionTime.java
@@ -1,16 +1,20 @@
 package com.example.user.danielbuzzer;
 
+import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * Created by User on 2015-10-03.
  */
-public class ReactionTime extends ReactionStatsTab{
+public class ReactionTime extends ReactionStatsTab implements Parcelable{
+
     public int reactiont;
+    private ArrayList<reactiont> tweets = new ArrayList<reactiont>();
 
     public ReactionTime(int reactiont) {
 
